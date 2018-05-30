732714857d9eb8b2f4a0ea8ab13cb70ae91bed97
David Kan <dkkan@uw.edu>
2018-05-29 19:29:32 -0700

    I leave a descriptive commit message as an exercise to the reader.

---
+++ myblog/production.py
@@ -0,0 +1,16 @@
+import os
+
+import dj_database_url
+
+from settings import *
+
+
+DATABASES = {
+    'default': dj_database_url.config(default='sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3'))
+    }
+
+DEBUG = False
+TEMPLATE_DEBUG = False
+ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOSTS'), 'localhost']
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+SECRET_KEY = os.environ.get('SECRET_KEY')
